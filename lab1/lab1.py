d979045a8d8700838aa75f11c3ff49ce095a31cc
Nalin Ahuja <nalinahuja22@gmail.com>
2021-09-18 21:21:28 -0400

    Project Backup

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -13,17 +13,22 @@ from tensorflow import keras
 # Seed Value
 SEED_VALUE = 1618
 
-# Model Structure Constants
+# General Neural Network Structure Constants
 INPUT_SIZE = 784
-HIDDEN_SIZE = 15
+HIDDEN_SIZE = 512
 OUTPUT_SIZE = 10
 
-# Model Training Constants
-NUM_EPOCHS = 5
-LEARNING_RATE = 0.1
+# Custom Neural Network Training Constants
+NN2L_NUM_EPOCHS = 15
+NN2L_USE_BATCHES = True
+NN2L_LEARNING_RATE = 0.001
+
+# Keras Neural Network Training Constants
+KERAS_NUM_EPOCHS = 15
+KERAS_LEARNING_RATE = 0.001
 
 # Selected Algorithm ("guesser", "custom_net", "tf_net")
-ALGORITHM = "tf_net"
+ALGORITHM = "guesser"
 
 # End Embedded Constants-----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -40,71 +45,141 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 # End Module Initialization--------------------------------------------------------------------------------------------------------------------------------------------
 
 class NeuralNetwork_2Layer():
-    def __init__(self, input_size, output_size, neurons_per_layer, activation = "sigmoid", learning_rate = 0.1):
+    def __init__(self, input_size, output_size, neurons_per_layer, learning_rate = 0.001):
         # Initialize Neural Network Attributes
-        self.input_size = inputSize
-        self.output_size = outputSize
+        self.input_size = input_size
+        self.output_size = output_size
         self.neurons_per_layer = neurons_per_layer
         self.learning_rate = learning_rate
-
-        # Set Activation Function
-        if (activation == "relu"):
-            self.__activation = self.__relu
-            self.__activation_prime = self.__relu_derivative
-        elif (activation == "sigmoid"):
-            self.__activation = self.__sigmoid
-            self.__activation_prime = self.__sigmoid_derivative
-        else:
-            # Throw Error Due To Invalid Activation Function
-            raise ValueError("activation function not recognized")
 
         # Initialize Weights Randomly
         self.W1 = np.random.randn(self.input_size, self.neurons_per_layer)
         self.W2 = np.random.randn(self.neurons_per_layer, self.output_size)
 
-    # ReLU Activation Function
-    def __relu(self, x):
-        # Compute ReLU Value At X
-        return (max(0, x))
-
-    # ReLU Activation Function Derivative
-    def __relu_derivative(self, x):
-        # Return Derivative Of ReLU
-        return ((1) if (x > 0) else (0))
-
     # Sigmoid Activation Function
     def __sigmoid(self, x):
-        # Compute Sigmoid Value At X
+        # Return Sigmoid Value At X
         return (1 / (1 + np.exp(-x)))
 
     # Sigmoid Activation Function Derivative
-    def __sigmoid_derivative(self, x):
+    def __sigmoid_prime(self, x):
         # Compute Sigmoid Value At X
         s = self.__sigmoid(x)
 
         # Return Derivative Of Sigmoid
-        return (s * (1 - s))
+        return (np.multiply(s, (1 - s)))
 
-    # Unrandomized Batch Generator For Mini-Batches
-    def __batchGenerator(self, l, n):
+    # Unrandomized Batch Generator For Mini Batches
+    def __batch_generator(self, x_vals, y_vals, n):
         # Iterate Over Training Samples
-        for i in range(0, len(l), n):
+        for i in range(0, x_vals.shape[0], n):
             # Return Slice Of Training Samples
-            yield l[i : i + n]
-
-    # Training with backpropagation
-    def train(self, xVals, yVals, epochs = 100000, minibatches = True, mbs = 100):
-        #TODO: Implement backprop. allow minibatches. mbs should specify the size of each minibatch.
-        pass
+            yield (x_vals[i : i + n], y_vals[i : i + n])
 
     # Forward Pass Function
     def __forward(self, input):
         # Calculate Sigmoid Values For Layers
-        layer1 = self.__activation(np.dot(input, self.W1))
-        layer2 = self.__activation(np.dot(layer1, self.W2))
+        layer1 = self.__sigmoid(np.dot(input, self.W1))
+        layer2 = self.__sigmoid(np.dot(layer1, self.W2))
 
         # Return Result
         return (layer1, layer2)
+
+    # Loss Function
+    def __loss(self, target, out):
+        # Calculate Squared Differences
+        loss = np.square(np.subtract(target, out))
+
+        # Scalar Divide Squared Differences
+        loss = np.divide(loss, 2)
+
+        # Sum Over All Squared Differences
+        loss = np.sum(loss)
+
+        # Return Loss
+        return (loss)
+
+    # Loss Function Derivative
+    def __loss_prime(self, target, out):
+        # Return Loss
+        return (np.subtract(out, target))
+
+    # Training With Backpropagation
+    def train(self, x_vals, y_vals, epochs = 10, mini_batches = True, batch_size = 100):
+        # Run Training Epochs
+        for i in range(1, epochs + 1):
+            # Display Status
+            print("\rEpoch %d/%d " % (i, epochs), end = "")
+
+            # Determine Training Strategy
+            if (mini_batches):
+                # Train Neural Network Using Mini Batches
+                for x_batch, y_batch in (self.__batch_generator(x_vals, y_vals, batch_size)):
+                    # Get Feed Forward Output
+                    l1_out, l2_out = self.__forward(x_batch)
+
+                    # Calculate Loss Derivative For Batch
+                    loss_prime = self.__loss_prime(y_batch, l2_out)
+
+                    # Calculate Activation Derivative For Batch
+                    logistic_prime = self.__sigmoid_prime(l2_out)
+
+                    # Calculate Output Layer Output Differences
+                    l2_diffs = np.multiply(loss_prime, logistic_prime)
+
+                    # Calculate Error Derivative For All Hidden Layer Neurons
+                    error_prime = np.dot(l2_diffs, np.transpose(self.W2))
+
+                    # Calculate Activation Derivative For Batch
+                    logistic_prime = self.__sigmoid_prime(l1_out)
+
+                    # Calculate Hidden Layer Output Differences
+                    l1_diffs = np.multiply(error_prime, logistic_prime)
+
+                    # Compute Output Layer Weight Adjustments
+                    l2_adj = np.matmul(np.transpose(l1_out), l2_diffs)
+
+                    # Adjust Output Layer Weights
+                    self.W2 -= np.multiply(l2_adj, self.learning_rate)
+
+                    # Compute Hidden Layer Weight Adjustments
+                    l1_adj = np.matmul(np.transpose(x_batch), l1_diffs)
+
+                    # Adjust Hidden Layer Weights
+                    self.W1 -= np.multiply(l1_adj, self.learning_rate)
+            else:
+                # Get Feed Forward Output
+                l1_out, l2_out = self.__forward(x_vals)
+
+                # Calculate Loss Derivative For Samples
+                loss_prime = self.__loss_prime(y_vals, l2_out)
+
+                # Calculate Activation Derivative For Samples
+                logistic_prime = self.__sigmoid_prime(l2_out)
+
+                # Calculate Output Layer Output Differences
+                l2_diffs = loss_prime * logistic_prime
+
+                # Calculate Error Derivative For All Hidden Layer Neurons
+                error_prime = np.dot(l2_diffs, np.transpose(self.W2))
+
+                # Calculate Activation Derivative For Samples
+                logistic_prime = self.__sigmoid_prime(l1_out)
+
+                # Calculate Hidden Layer Output Differences
+                l1_diffs = error_prime * logistic_prime
+
+                # Compute Output Layer Weight Adjustments
+                l2_adj = np.matmul(np.transpose(l1_out), l2_diffs)
+
+                # Adjust Output Layer Weights
+                self.W2 -= np.multiply(l2_adj, self.learning_rate)
+
+                # Compute Hidden Layer Weight Adjustments
+                l1_adj = np.matmul(np.transpose(x_vals), l1_diffs)
+
+                # Adjust Hidden Layer Weights
+                self.W1 -= np.multiply(l1_adj, self.learning_rate)
 
     # Prediction Function
     def predict(self, xVals):
@@ -116,52 +191,69 @@ class NeuralNetwork_2Layer():
 
 # End Neural Network Layer Class---------------------------------------------------------------------------------------------------------------------------------------
 
-def get_raw_data():
-    # Fetch MNIST Dataset From Tensorflow Imporrt
-    mnist = tf.keras.datasets.mnist
+def encode_preds(preds):
+    # Initialize Encoded Predictions Representation
+    enc = np.zeros(preds.shape)
 
+    # Determine Indicies With Maximum Probability
+    mpi = np.argmax(preds, axis = 1)
+
+    # Iterate Over Predictions
+    for i in range(preds.shape[0]):
+        # Set Position Of Maximum Probability
+        enc[i][mpi[i]] = 1
+
+    # Return Encoded Predictions Representation
+    return (enc)
+
+# End Utility Functions------------------------------------------------------------------------------------------------------------------------------------------------
+
+def get_data():
     # Load Data From MNIST Dataset
-    (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
     # Display Information About Dataset
-    print("Shape of xTrain dataset: %s" % str(xTrain.shape))
-    print("Shape of yTrain dataset: %s" % str(yTrain.shape))
-    print("Shape of xTest dataset: %s" % str(xTest.shape))
-    print("Shape of yTest dataset: %s" % str(yTest.shape))
+    print("Shape of x_train dataset: %s" % (str(x_train.shape)))
+    print("Shape of y_train dataset: %s" % (str(y_train.shape)))
+    print("Shape of x_test dataset: %s" % (str(x_test.shape)))
+    print("Shape of y_test dataset: %s" % (str(y_test.shape)))
+    print()
 
     # Return Data
-    return ((xTrain, yTrain), (xTest, yTest))
+    return ((x_train, y_train), (x_test, y_test))
 
-def preprocess_data(raw):
+def process_data(raw):
     # Unpack Data From Raw Input
-    ((xTrain, yTrain), (xTest, yTest)) = raw
+    ((x_train, y_train), (x_test, y_test)) = raw
 
-    # Normalize Input Data To Range
-    xTrainP = np.divide(xTrain, 255)
-    xTestP = np.divide(xTest, 255)
+    # Normalize Input Data
+    x_train = np.divide(x_train, 255, dtype = np.float16)
+    x_test = np.divide(x_test, 255, dtype = np.float16)
+
+    # Reshape Input Data
+    x_train = x_train.reshape(-1, 28 ** 2)
+    x_test = x_test.reshape(-1, 28 ** 2)
 
     # Process Integer Arrays Into Binary Class Matrices
-    yTrainP = keras.utils.to_categorical(yTrain, OUTPUT_SIZE)
-    yTestP = keras.utils.to_categorical(yTest, OUTPUT_SIZE)
+    y_train = keras.utils.to_categorical(y_train, OUTPUT_SIZE)
+    y_test = keras.utils.to_categorical(y_test, OUTPUT_SIZE)
 
     # Display Information About Dataset
-    print("New shape of xTrain dataset: %s" % str(xTrainP.shape))
-    print("New shape of yTrain dataset: %s" % str(yTrainP.shape))
-    print("New shape of xTest dataset: %s" % str(xTestP.shape))
-    print("New shape of yTest dataset: %s" % str(yTestP.shape))
+    print("New shape of x_train dataset: %s" % (str(x_train.shape)))
+    print("New shape of y_train dataset: %s" % (str(y_train.shape)))
+    print("New shape of x_test dataset: %s" % (str(x_test.shape)))
+    print("New shape of y_test dataset: %s" % (str(y_test.shape)))
+    print()
 
     # Return Preprocessed Data
-    return ((xTrain, yTrainP), (xTest, yTestP))
+    return ((x_train, y_train), (x_test, y_test))
 
 def train_model(data):
     # Unpack Training Data
-    xTrain, yTrain = data
+    x_train, y_train = data
 
     # Run Training Algorithm
     if (ALGORITHM == "guesser"):
-        # Display Status
-        print("Using guesser algorithm...")
-
         # Return No Model
         return (None)
     elif (ALGORITHM == "custom_net"):
@@ -169,10 +261,13 @@ def train_model(data):
         print("Training custom neural network...")
 
         # Initialize New Neural Network Instance
-        model = NeuralNetwork_2Layer(INPUT_SIZE, OUTPUT_SIZE, NUM_NEURON, activation = "sigmoid")
+        model = NeuralNetwork_2Layer(INPUT_SIZE, OUTPUT_SIZE, HIDDEN_SIZE, learning_rate = NN2L_LEARNING_RATE)
 
         # Train Model With Training Data
-        model.train(xTrain, yTrain, epochs = NUM_EPOCHS)
+        model.train(x_train, y_train, epochs = NN2L_NUM_EPOCHS, mini_batches = NN2L_USE_BATCHES)
+
+        # Print Separator
+        print("\n" * 2, end = "")
 
         # Return Model
         return (model)
@@ -180,14 +275,11 @@ def train_model(data):
         # Display Status
         print("Training Tensorflow neural network...")
 
-        # Initialize New Sequential Instance
+        # Initialize New Sequential Model Instance
         model = keras.Sequential()
 
-        # Initialize Loss Function
-        loss_func = keras.losses.categorical_crossentropy
-
-        # Initialize Model Optimizer
-        opt_func = tf.keras.optimizers.Adam(learning_rate = LEARNING_RATE)
+        # Add Flattening Layer To Model
+        model.add(keras.layers.Flatten())
 
         # Add Neuron Hidden Layer To Model
         model.add(keras.layers.Dense(HIDDEN_SIZE, input_shape = [INPUT_SIZE], activation = tf.nn.relu))
@@ -195,11 +287,20 @@ def train_model(data):
         # Add Neuron Output Layer To Model
         model.add(keras.layers.Dense(OUTPUT_SIZE, input_shape = [HIDDEN_SIZE], activation = tf.nn.softmax))
 
+        # Initialize Loss Function
+        loss_func = keras.losses.categorical_crossentropy
+
+        # Initialize Model Optimizer
+        opt_func = tf.optimizers.Adam(learning_rate = KERAS_LEARNING_RATE)
+
         # Compile Model
-        model.compile(optimizer = opt_func, loss = loss_func)
+        model.compile(loss = loss_func, optimizer = opt_func, metrics = ["accuracy"])
 
         # Train Model
-        model.fit(xTrain, yTrain, epochs = NUM_EPOCHS)
+        model.fit(x_train, y_train, epochs = KERAS_NUM_EPOCHS)
+
+        # Print Separator
+        print("\n" * 1, end = "")
 
         # Return Model
         return (model)
@@ -210,86 +311,104 @@ def train_model(data):
 def run_model(data, model):
     if (ALGORITHM == "guesser"):
         # Display Status
-        print("Running guesser algorithm...")
+        print("Running guesser algorithm...\n")
 
-        # Initialize Answer Vector
-        ans = []
+        # Initialize Predictions Vector
+        preds = []
 
-        # Iterate Over xTest Sample
-        for entry in (data):
+        # Iterate Over Data Sample
+        for i in range(data.shape[0]):
             # Initialize Base Prediction Vector
-            pred = [0] * 10
+            pred = np.zeros(OUTPUT_SIZE)
 
             # Randomly Set Class Label
             pred[random.randint(0, 9)] = 1
 
-            # Append Predicted Class To Answer Vector
-            ans.append(pred)
+            # Append Predicted Class To Predictions Vector
+            preds.append(pred)
 
-        # Return Prediction
-        return (np.array(ans))
+        # Return Predictions
+        return (np.array(preds))
     elif (ALGORITHM == "custom_net"):
         # Display Status
-        print("Running custom neural network...")
+        print("Running custom neural network...\n")
 
-        # Run Custom Model
+        # Run Custom Model On Data
         preds = model.predict(data)
 
-        # Return Prediction
+        # One Hot Encode Predictions
+        preds = encode_preds(preds)
+
+        # Return Predictions
         return (np.array(preds))
     elif (ALGORITHM == "tf_net"):
         # Display Status
-        print("Running Tensorflow neural network...")
+        print("Running Tensorflow neural network...\n")
 
-        # Run Keras Model
+        # Run Keras Model On Data
         preds = model.predict(data)
 
-        # TODO: one hot encoding
+        # One Hot Encode Predictions
+        preds = encode_preds(preds)
 
-        # Return Prediction
+        # Return Predictions
         return (np.array(preds))
     else:
         # Throw Error Due To Invalid Algorithm
         raise ValueError("algorithm not recognized")
 
-def eval_results(data, preds):
-    # Unpack Data
-    xTest, yTest = data
+    # Print Separator
+    print()
+
+def eval_results(data, y_preds):
+    # Unpack Output Test Data
+    _, y_test = data
+
+    # Format Test Data
+    y_test = np.argmax(y_test, axis = 1)
+
+    # Format Prediction Data
+    y_preds = np.argmax(y_preds, axis = 1)
 
     # Initialize Accuracy
-    acc = 0
+    accuracy = 0
 
     # Iterate Over Predicted Values
-    for i in range(preds.shape[0]):
+    for i in range(y_preds.shape[0]):
         # Verify Predicted Values Match Expected Values
-        if (np.array_equal(preds[i], yTest[i])):
+        if (y_test[i] == y_preds[i]):
             # Increment Accuracy Metric
-            acc += 1
+            accuracy += 1
 
     # Calculate Accuracy
-    acc /= preds.shape[0]
+    accuracy /= y_preds.shape[0]
 
-    # TODO: Add F1 score and confusion matrix
+    # Initialize Confusion Matrix Representation
+    confusion = np.zeros((OUTPUT_SIZE, OUTPUT_SIZE))
 
-    # Initialize Prediction Metrics
-    tp = tn = fp = fn = 0
+    # Iterate Over Predicted Values
+    for i in range(y_preds.shape[0]):
+        # Update Confusion Matrix
+        confusion[y_test[i]][y_preds[i]] += 1
 
-    # # Iterate Over Predicted Values
-    # for i in range(preds.shape[0]):
-    #     # Verify Predicted Values Match Expected Value
+    # Initialize Class Accuracy Indications
+    tp = fp = fn = tn = 0
 
     # Display Classifier Metrics
-    print("Classifier algorithm: %s" % ALGORITHM)
-    print("Classifier accuracy: %f%%" % (acc * 100))
+    print("Classifier algorithm: %s" % (ALGORITHM))
+    print("Classifier accuracy: %f%%" % (accuracy * 100))
 
 # End Pipeline Functions-----------------------------------------------------------------------------------------------------------------------------------------------
 
 if (__name__ == '__main__'):
-    # Get Raw Data
-    raw = get_raw_data()
+    # Ignore Numpy Warnings
+    np.seterr(all = "ignore")
 
-    # Preprocess Raw Data
-    data = preprocess_data(raw)
+    # Get Raw Data
+    raw = get_data()
+
+    # Process Raw Data
+    data = process_data(raw)
 
     # Train Model On Raw Data
     model = train_model(data[0])
