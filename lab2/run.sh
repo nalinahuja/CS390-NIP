eccbae21a0b0c27c00cf4d89ad817c78f5111fb2
Nalin Ahuja <nalinahuja22@gmail.com>
2021-10-04 15:30:06 -0400

    Project Backup

---
+++ lab2/run.sh
@@ -0,0 +1,25 @@
+# Developed By Nalin Ahuja, ahuja15
+
+# Algorithm List
+ALGORITHMS=("guesser" "tf_net" "tf_conv")
+
+# Datasets List
+DATASETS=("mnist_d" "mnist_f" "cifar_10" "cifar_100_f" "cifar_100_c")
+
+# Get Terminal Columns
+columns=$(command tput cols)
+
+# Separator Format
+sfmt=$(command printf "%-${columns}s")
+
+# Iterate Over Algorithms
+for algorithm in ${ALGORITHMS[@]}; do
+  # Iterate Over Datasets
+  for dataset in ${DATASETS[@]}; do
+    # Run Program With Arguments
+    command python3 ./lab2.py ${algorithm} ${dataset}
+
+    # Print Separator
+    command echo -e "\n${sfmt// /-}\n"
+  done
+done
